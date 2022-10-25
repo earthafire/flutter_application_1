2d4147b425803b52d3fa644869d0ad266539fcf5
Earthafire <resiget@gmail.com>
2022-10-24 20:21:54 -0500

    changed init var to 4

--- lib/main.dart
+++ lib/main.dart
@@ -48,7 +48,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 3;
+  int _counter = 4;
 
   void _incrementCounter() {
     setState(() {
